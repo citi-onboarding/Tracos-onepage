7c9bd3629df46bbf2a2e120ba217250ce9ae652c
cxmedeiros <cxm@cin.ufpe.br>
2022-05-25 18:12:26 -0300

    feature([feature/tattooists]):tattooists backend

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,13 +1,19 @@
 import express from 'express';
-import UserController from '@controllers/UserController'
+import UserController from '@controllers/UserController';
+import TattooistsController from '@controllers/TattooistsControllers';
 
 const routes = express.Router();
 const userController = new UserController();
+const tattooistsController = new TattooistsController();
 
 routes.post('/user', userController.create);
 routes.get('/user', userController.get);
 routes.delete('/user/:id', userController.delete);
 routes.put('/user/:id', userController.update);
 
+routes.post('/tattooists', tattooistsController.create);
+routes.get('/tattooists', tattooistsController.get);
+routes.delete('/tattooists/:id', tattooistsController.delete);
+routes.put('/tattooists/:id', tattooistsController.update);
 
 export default routes;
