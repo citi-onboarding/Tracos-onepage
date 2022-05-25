549bcfb8e7a89024418a15c17eee476d7cde2c74
titi-lima <97147969+titi-lima@users.noreply.github.com>
2022-05-25 13:06:43 -0300

    update routes.ts

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,13 +1,19 @@
 import express from 'express';
 import UserController from '@controllers/UserController'
+import VantagensPessoasTatuadasController from '@controllers/VantagensPessoasTatuadasController'
 
 const routes = express.Router();
 const userController = new UserController();
+const vantagensPessoasTatuadasController = new VantagensPessoasTatuadasController();
 
 routes.post('/user', userController.create);
 routes.get('/user', userController.get);
 routes.delete('/user/:id', userController.delete);
 routes.put('/user/:id', userController.update);
 
+routes.post('/vantagensPessoasTatuadas', vantagensPessoasTatuadasController.create);
+routes.get('/vantagensPessoasTatuadas', vantagensPessoasTatuadasController.get);
+routes.delete('/vantagensPessoasTatuadas/:id', vantagensPessoasTatuadasController.delete);
+routes.put('/vantagensPessoasTatuadas/:id', vantagensPessoasTatuadasController.update);
 
 export default routes;
