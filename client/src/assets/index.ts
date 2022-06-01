af6d1603a921ed057edbcbf27e965ea1bd91b202
marinavelosom <engmarinamelo@gmail.com>
2022-06-01 09:11:26 -0300

    feature[feature/footerTRS]: what I did: - export assets

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -1,5 +1,22 @@
-import LogoCITi from './logo.png';
+import Heart from './Heart.svg';
+import InstagramIcon from './Instagram.svg';
+import TwitterIcon from './Twitter.svg'
+import CitiIcon from './CITi.svg'
+
+import NoirProLightRegularWoff from './noir-pro/NoirPro-Webfont/NoirPro-Light.woff';
+import NoirProLightRegularWoff2 from './noir-pro/NoirPro-Webfont/NoirPro-Italic.woff2';
+import NoirProBoldItalicRegularWoff from './noir-pro/NoirPro-Webfont/NoirPro-BoldItalic.woff'
+import NoirProBoldItalicRegularWoff2 from './noir-pro/NoirPro-Webfont/NoirPro-BoldItalic.woff2'
 
 export {
-    LogoCITi
+    CitiIcon,
+    Heart,
+    InstagramIcon,
+    TwitterIcon,
+
+    NoirProBoldItalicRegularWoff,
+    NoirProBoldItalicRegularWoff2,
+    NoirProLightRegularWoff,
+    NoirProLightRegularWoff2
 };
+
