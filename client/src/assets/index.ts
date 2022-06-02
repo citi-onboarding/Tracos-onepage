8cc5b811aa497a80d494402d9ec9e462e72b3ce7
titi-lima <97147969+titi-lima@users.noreply.github.com>
2022-06-02 14:32:15 -0300

    feature([feature/tattooistsTRS]): added fonts and laid groundwork for integration

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -3,12 +3,12 @@ import Arrow from './arrow.svg';
 import Mylena from './mylena.png';
 import Pedro from './pedro.png';
 import Samuel from './samuel.png';
-import NoirProRegularWoff2 from './noir-pro/NoirPro-Webfont/NoirPro-Regular.woff2'
-import NoirProRegularWoff from './noir-pro/NoirPro-Webfont/NoirPro-Regular.woff'
-import NoirProMediumWoff2 from './noir-pro/NoirPro-Webfont/NoirPro-Medium.woff2'
-import NoirProMediumWoff from './noir-pro/NoirPro-Webfont/NoirPro-Medium.woff'
-import NoirProMediumItalicWoff2 from './noir-pro/NoirPro-Webfont/NoirPro-MediumItalic.woff2'
-import NoirProMediumItalicWoff from './noir-pro/NoirPro-Webfont/NoirPro-MediumItalic.woff'
+import NoirProRegularWoff2 from './noir-pro/NoirPro-Webfont/NoirPro-Regular.woff2';
+import NoirProRegularWoff from './noir-pro/NoirPro-Webfont/NoirPro-Regular.woff';
+import NoirProMediumWoff2 from './noir-pro/NoirPro-Webfont/NoirPro-Medium.woff2';
+import NoirProMediumWoff from './noir-pro/NoirPro-Webfont/NoirPro-Medium.woff';
+import NoirProMediumItalicWoff2 from './noir-pro/NoirPro-Webfont/NoirPro-MediumItalic.woff2';
+import NoirProMediumItalicWoff from './noir-pro/NoirPro-Webfont/NoirPro-MediumItalic.woff';
 
 
 export {
--- client/src/pages/tattooists/index.tsx
+++ client/src/pages/tattooists/index.tsx
@@ -1,10 +1,16 @@
 import React from "react";
-import { useEffect } from 'react';
+import { useEffect } from "react";
 import { Mylena, Pedro, Samuel, Arrow } from "../../assets";
-import { Artists, Text } from './styles';
+import { Artists, Text } from "./styles";
+import axios from "axios";
 
 export const Tattooists: React.FC = () => {
   useEffect(() => {
+    const Tattooist: { image: string; title: string; description: string }[] = [
+      { image: Samuel, title: "Samuel Reis", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec faucibus odio sit amet sollicitudin porta. Integer in elit ut enim accumsan pellentesque. Phasellus sit amet tortor orci. Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
+      { image: Mylena, title: "Mylena Soares", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis distinctio natus facere qui eum, odio atque corporis eaque veniam magnam necessitatibus aut expedita illo, laudantium deleniti perferendis vel? Similique, culpa?" },
+      { image: Pedro, title: "Pedro Basílio", description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem repellendus suscipit fugit corrupti quo tempora! Animi dolorum ducimus dolor error quae voluptatum consequuntur voluptatibus sunt, beatae cupiditate culpa, excepturi rem" },
+    ];
     const next = document.getElementById("next");
     const prev = document.getElementById("prev");
     const nome = document.getElementById("name");
@@ -13,69 +19,96 @@ export const Tattooists: React.FC = () => {
     const ourTattooistAux = document.getElementById("ourTattooistAux") as HTMLImageElement;
     const description = document.getElementById("description");
     const descriptionAux = document.getElementById("descriptionAux");
-    const isAnyUndefined = !(next && nome && ourTattooistAux && nomeAux && ourTattooist && prev && description && descriptionAux)
-    let currNome = nome!.innerText;
-    
+    const isAnyUndefined = !(next && nome && ourTattooistAux && nomeAux && ourTattooist && prev && description && descriptionAux);
+    let index = 0;
     if (!isAnyUndefined) {
       next.addEventListener("click", () => {
-          switch (currNome) {
-            case "Samuel Reis":
-              ourTattooistAux.src = Mylena;
-              currNome = nomeAux.innerText = "Mylena Soares";
-              descriptionAux.innerText = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis distinctio natus facere qui eum, odio atque corporis eaque veniam magnam necessitatibus aut expedita illo, laudantium deleniti perferendis vel? Similique, culpa?"
+        console.log(index);
+        switch (index % 2) {
+          case 0:
+            if (index < Tattooist.length - 1) {
+              ++index;
+              ourTattooistAux.src = Tattooist[index].image;
+              nomeAux.innerText = Tattooist[index].title;
+              descriptionAux.innerText = Tattooist[index].description;
               description.classList.add("hidden");
               descriptionAux.classList.remove("hidden");
               ourTattooist.classList.add("hidden");
               ourTattooistAux.classList.remove("hidden");
               nomeAux.classList.remove("hidden");
               nome.classList.add("hidden");
-              prev.classList.remove("inactive");
-              prev.classList.add("active");
-              break;
-            case "Mylena Soares":
-              ourTattooist.src = Pedro;
-              currNome = nome.innerText = "Pedro Basílio";
-              description.innerText = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem repellendus suscipit fugit corrupti quo tempora! Animi dolorum ducimus dolor error quae voluptatum consequuntur voluptatibus sunt, beatae cupiditate culpa, excepturi rem."
+              if (index === 1) {
+                prev.classList.remove("inactive");
+                prev.classList.add("active");
+              }
+              if (index === Tattooist.length - 1) {
+                next.classList.add("inactive");
+                next.classList.remove("active");
+              }
+            }
+            break;
+          case 1:
+            if (index < Tattooist.length - 1) {
+              ++index;
+              ourTattooist.src = Tattooist[index].image;
+              nome.innerText = Tattooist[index].title;
+              description.innerText = Tattooist[index].description;
               descriptionAux.classList.add("hidden");
               description.classList.remove("hidden");
               ourTattooistAux.classList.add("hidden");
               ourTattooist.classList.remove("hidden");
-              next.classList.add("inactive");
-              next.classList.remove("active");
               nome.classList.remove("hidden");
               nomeAux.classList.add("hidden");
-              break;
-          }
+              if (index === Tattooist.length - 1) {
+                next.classList.add("inactive");
+                next.classList.remove("active");
+              }
+            }
+          break;
+        }
       });
       prev.addEventListener("click", () => {
-        switch (currNome) {
-          case "Samuel Reis":
-            break;
-          case "Mylena Soares":
-            currNome = nome.innerText = "Samuel Reis";
-            ourTattooist.src = Samuel;
-            description.innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec faucibus odio sit amet sollicitudin porta. Integer in elit ut enim accumsan pellentesque. Phasellus sit amet tortor orci. Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        console.log(index);
+        switch (index % 2) {
+          case 1:
+            --index;
+            nome.innerText = Tattooist[index].title;
+            ourTattooist.src = Tattooist[index].image;
+            description.innerText = Tattooist[index].description;
             descriptionAux.classList.add("hidden");
             description.classList.remove("hidden");
             ourTattooistAux.classList.add("hidden");
             ourTattooist.classList.remove("hidden");
             nome.classList.remove("hidden");
             nomeAux.classList.add("hidden");
-            prev.classList.add("inactive");
-            prev.classList.remove("active");
+            if (index === 0) {
+              prev.classList.add("inactive");
+              prev.classList.remove("active");
+            }
+            if (index === Tattooist.length - 2) {
+              next.classList.remove("inactive");
+              next.classList.add("active");
+            }
             break;
-          case "Pedro Basílio":
-            ourTattooistAux.src = Mylena;
-            currNome = nomeAux.innerText = "Mylena Soares";
-            descriptionAux.innerText = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis distinctio natus facere qui eum, odio atque corporis eaque veniam magnam necessitatibus aut expedita illo, laudantium deleniti perferendis vel? Similique, culpa?"
-            description.classList.add("hidden");
-            descriptionAux.classList.remove("hidden");
-            ourTattooist.classList.add("hidden");
-            ourTattooistAux.classList.remove("hidden");
-            nomeAux.classList.remove("hidden");
-            nome.classList.add("hidden");
-            next.classList.remove("inactive");
-            next.classList.add("active");
+          case 0:
+            if (index !== 0) {
+              --index;
+              ourTattooistAux.src = Tattooist[index].image;
+              nomeAux.innerText = Tattooist[index].title;
+              descriptionAux.innerText = Tattooist[index].description;
+              description.classList.add("hidden");
+              descriptionAux.classList.remove("hidden");
+              ourTattooist.classList.add("hidden");
+              ourTattooistAux.classList.remove("hidden");
+              nomeAux.classList.remove("hidden");
+              nome.classList.add("hidden");
+              next.classList.remove("inactive");
+              next.classList.add("active");
+              if (index === Tattooist.length - 2) {
+                next.classList.remove("inactive");
+                next.classList.add("active");
+              }
+            }
             break;
         }
       });
@@ -91,12 +124,16 @@ export const Tattooists: React.FC = () => {
         id="ourTattooist"
       />
       <Text>
-        <h1 id="ourartists">Nossos <span>tatuadores</span></h1>
+        <h1 id="ourartists">
+          Nossos <span>tatuadores</span>
+        </h1>
         <br />
         <br />
         <br />
         <h2 id="name">Samuel Reis</h2>
-        <h2 id="nameAux" className="hidden">Samuel Reis</h2>
+        <h2 id="nameAux" className="hidden">
+          Samuel Reis
+        </h2>
         <br />
         <p id="description">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
@@ -122,4 +159,4 @@ export const Tattooists: React.FC = () => {
       </div>
     </Artists>
   );
-}
+};
