f73bdb9d0a1643e13a33a30275e8a4af9ba6b515
marinavelosom <engmarinamelo@gmail.com>
2022-06-07 14:25:07 -0300

    feature[feature/tattooed-people-advantagesTRS]: integrates front end and back end

--- client/src/components/ContainerTattooedPeopleAdvantages/index.tsx
+++ client/src/components/ContainerTattooedPeopleAdvantages/index.tsx
@@ -2,22 +2,45 @@ import React from "react";
 import { TattooedPeopleAdvantages } from "./TattooedPeopleAdvantages";
 import { ImageAdvantage1, ImageAdvantage2, ImageAdvantage3 } from '../../assets'
 import { Container } from "./style";
+import { useState, useEffect } from "react";
+import api from "../../services/api";
+
+type tattoedPeopleInfos = {
+    image: string;
+    title: string;
+    description: string;
+}
 
 export const ContainerTattooedPeopleAdvantages: React.ElementType = () =>{
+    const [infos, setInfos] = useState<tattoedPeopleInfos[]>();
+
+    const getInfos = async () => {
+        const response = await api.get('/tattooedPeopleAdvantages');
+        setInfos(response.data);
+        console.log(response.data);
+    }
+    
+    useEffect (() =>{
+        getInfos();
+    }, [])
+
     return(
         <Container>
             <TattooedPeopleAdvantages 
-            image={ImageAdvantage1} 
-            title="Feature 00" 
-            text="Lorem ipsum dolor sit amet, cons ectetur adipiscing elit. Donec faucibus odio sit amet sollicitudin porta."/>
+                image = {infos&&infos[0].image} 
+                title = {infos&&infos[0].title}
+                text = {infos&&infos[0].description}
+            />
             <TattooedPeopleAdvantages 
-            image={ImageAdvantage2} 
-            title="Feature 00" 
-            text="Lorem ipsum dolor sit amet, cons ectetur adipiscing elit. Donec faucibus odio sit amet sollicitudin porta."/>
+                image = {infos&&infos[1].image} 
+                title = {infos&&infos[1].title}
+                text = {infos&&infos[1].description}
+            />
             <TattooedPeopleAdvantages 
-            image={ImageAdvantage3} 
-            title="Feature 00" 
-            text="Lorem ipsum dolor sit amet, cons ectetur adipiscing elit. Donec faucibus odio sit amet sollicitudin porta."/>
+                image = {infos&&infos[2].image} 
+                title = {infos&&infos[2].title}
+                text = {infos&&infos[2].description}
+            />
       </Container>
     )
-} 
+};
