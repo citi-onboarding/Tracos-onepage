1ab307d56b00b8f697647d9930c288aad0690ba3
camila <cxm@cin.ufpe.br>
2022-06-07 22:41:40 -0300

    feature[feature/tattoo-artists-advantagesTRS]: integration

--- client/src/pages/TattooistsAdvantages/index.tsx
+++ client/src/pages/TattooistsAdvantages/index.tsx
@@ -1,34 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from "../../services/api";
 import { Card } from "../../components";
 import { Title, Container, SectionContainer } from './styles';
-import { Foto1, Foto2, Foto3 } from "../../assets";
 
-
+type tattooistsAdvantages = {
+    title: string;
+    img: string;
+    description: string;
+};
 
 export const TattooistsAdvantages: React.FC = () =>{
+    const [infos, setInfos] = useState <tattooistsAdvantages[]> ();
+
+    const getInfos = async () => {
+        const response = await api.get('/tattooArtistsAdvantages');
+        setInfos(response.data);
+        console.log(response.data);
+    }
+
+    useEffect (() => {
+        getInfos();
+    }, []) 
+
     return(
 
         <SectionContainer>
             <Title>Para quem <i>marca</i></Title>
             <Container>
                 <Card
-                title = "Feature 00"
-                image = {Foto2}
-                description = "Lorem ipsum dolor sit amet, cons ectetur adipiscing elit. Donec faucibus odio sit amet sollicitudin porta."
+                title = {infos&&infos[0].title}
+                image = {infos&&infos[0].img}
+                description = {infos&&infos[0].description}
                 />
                 <Card
-                title = "Feature 00"
-                image = {Foto1}
-                description = "Lorem ipsum dolor sit amet, cons ectetur adipiscing elit. Donec faucibus odio sit amet sollicitudin porta."
+                title = {infos&&infos[1].title}
+                image = {infos&&infos[1].img}
+                description = {infos&&infos[1].description}
                 />
                 <Card
-                title = "Feature 00"
-                image = {Foto3}
-                description = "Lorem ipsum dolor sit amet, cons ectetur adipiscing elit. Donec faucibus odio sit amet sollicitudin porta."
+                title = {infos&&infos[2].title}
+                image = {infos&&infos[2].img}
+                description = {infos&&infos[2].description}
                 />
             </Container>
         </SectionContainer>
-
-        
+ 
     )
 }
