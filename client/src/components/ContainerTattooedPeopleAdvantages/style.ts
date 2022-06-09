a0777142d45279d5d76f41ff0319adeae41abedd
marinavelosom <engmarinamelo@gmail.com>
2022-06-09 19:36:24 -0300

    feature[tattooed-people-advantagesTRS]: merge changes

--- client/src/components/ContainerTattooedPeopleAdvantages/style.ts
+++ client/src/components/ContainerTattooedPeopleAdvantages/style.ts
@@ -4,7 +4,6 @@ export const Container =  styled.div`
     display: flex;
     justify-content: center;
     gap: 70px;
-    margin-top: 56px;
     @media (max-width: 1000px){ 
         display: grid;
         gap: 24px;
--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -2,11 +2,15 @@ import Navbar from './navbar/navbar'
 import Contact from './contact/contact'
 import TattooistsAdvantages from './TattooistsAdvantages'
 import { Tattooists } from './tattooists';
+import { ContainerTattooedPeopleAdvantages } from '../components/ContainerTattooedPeopleAdvantages'
+import { TattooedPeopleAdvantagesTitle } from './ContainerTattooedPeopleAdvantages/TattooedPeopleAdvantages';
 
 export { 
     Navbar,
     Contact,
     TattooistsAdvantages,
-    Tattooists
+    Tattooists, 
+    ContainerTattooedPeopleAdvantages, 
+    TattooedPeopleAdvantagesTitle
 }
 
